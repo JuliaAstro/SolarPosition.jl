2a36a19edb1b491d3b0b297176b0a84c95b8f9b7
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-12-04 18:58:36 -0500

    Refactor the MTK component to be a bit more idiomatic

--- ext/SolarPositionModelingToolkitExt.jl
+++ ext/SolarPositionModelingToolkitExt.jl
@@ -23,7 +23,7 @@ get_zenith(pos::SolPos) = pos.zenith
 get_elevation(pos::AbstractApparentSolPos) = pos.apparent_elevation
 get_zenith(pos::AbstractApparentSolPos) = pos.apparent_zenith
 
-Symbolics.@register_symbolic seconds_to_datetime(t_sec, t0::DateTime)
+Symbolics.@register_symbolic seconds_to_datetime(t_sec, t0::DateTime)::DateTime
 Symbolics.@register_symbolic solar_position(
     observer::Observer,
     time::DateTime,
@@ -31,20 +31,25 @@ Symbolics.@register_symbolic solar_position(
     refraction::RefractionAlgorithm,
 )
 
-Symbolics.@register_symbolic get_azimuth(pos)
-Symbolics.@register_symbolic get_elevation(pos)
-Symbolics.@register_symbolic get_zenith(pos)
+Symbolics.@register_symbolic get_azimuth(pos)::Real
+Symbolics.@register_symbolic get_elevation(pos)::Real
+Symbolics.@register_symbolic get_zenith(pos)::Real
 
-function SolarPositionBlock(; name)
-    @parameters t0::DateTime [tunable = false] observer::Observer [tunable = false]
-    @parameters algorithm::SolarAlgorithm = PSA() [tunable = false]
-    @parameters refraction::RefractionAlgorithm = NoRefraction() [tunable = false]
+function SolarPositionBlock(; name; t0, observer, algorithm=PSA(), refraction=NoRefraction())
+    @parameters t0::DateTime = t0[tunable = false] 
+    @parameters observer::Observer = observer [tunable = false]
+    @parameters algorithm::SolarAlgorithm = algorithm [tunable = false]
+    @parameters refraction::RefractionAlgorithm = refraction [tunable = false]
 
     @variables azimuth(t) [output = true]
     @variables elevation(t) [output = true]
     @variables zenith(t) [output = true]
 
-    time_expr = Symbolics.term(seconds_to_datetime, t, t0; type = DateTime)
+    # Ideally this should be expressed by a symbolic equation
+    # but because it would involve some amount of type manipulation,
+    # we can keep it this way for now.
+    # But this might cause the position to be recalculated thrice (I think)
+    time_expr = seconds_to_datetime(t, t0)
     pos = solar_position(observer, time_expr, algorithm, refraction)
 
     eqs = [
@@ -53,7 +58,7 @@ function SolarPositionBlock(; name)
         zenith ~ get_zenith(pos),
     ]
 
-    return System(eqs, t; name = name)
+    return System(eqs, t; name = name, vars = [azimuth, elevation, zenith], params = [t0, observer, algorithm, refraction])
 end
 
 end # module SolarPositionModelingToolkitExt

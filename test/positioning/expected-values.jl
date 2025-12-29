e12b9cc885e5ec5dc81253d43745d707124506c4
Stefan de Lange <langestefan@msn.com>
2025-12-29 19:56:18 +0100

    Re-organize tests

--- test/positioning/expected-values.jl
+++ test/positioning/expected-values.jl
@@ -1,5 +1,109 @@
 """Expected values for algorithm tests (shared helper functions)"""
 
+using DataFrames
+using TimeZones
+using Dates
+
+function test_conditions()
+    inputs = DataFrame(
+        time = [
+            "2020-10-17T12:30+00:00",
+            "2020-10-17T12:30:10+00:00",
+            "2020-10-17T12:30+02:00",
+            "2020-10-17T12:30-02:00",
+            "2020-02-29T12:30+00:00",
+            "2020-10-17T12:30+00:00",
+            "2020-10-17T12:30+00:00",
+            "2020-10-17T12:30+00:00",
+            "2020-10-17T12:30+00:00",
+            "2020-10-17T12:30+00:00",
+            "2020-10-17T12:30+00:00",
+            "2020-10-17T12:30+00:00",
+            "1800-10-17T12:30+00:00",
+            "2200-10-17T12:30+00:00",
+            "2020-10-17T03:30+00:00",
+            "2020-10-17T05:50+00:00",
+            "2020-10-17T12:30+00:00",
+            "2020-10-17T12:30+00:00",
+            "2020-10-17T12:30+00:00",
+        ],
+        latitude = [
+            45.0,
+            45.0,
+            45.0,
+            45.0,
+            45.0,
+            90.0,
+            0.0,
+            -90.0,
+            -45.0,
+            45.0,
+            45.0,
+            45.0,
+            45.0,
+            45.0,
+            45.0,
+            45.0,
+            45.0,
+            45.0,
+            45.0,
+        ],
+        longitude = [
+            10.0,
+            10.0,
+            10.0,
+            10.0,
+            10.0,
+            10.0,
+            10.0,
+            10.0,
+            10.0,
+            -15.0,
+            -180.0,
+            180.0,
+            10.0,
+            10.0,
+            10.0,
+            10.0,
+            10.0,
+            10.0,
+            10.0,
+        ],
+        altitude = [
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            missing,
+            0.0,
+            -100.0,
+            4000.0,
+        ],
+    )
+
+    # parse times as ZonedDateTime
+    inputs.time = [
+        try
+            ZonedDateTime(t, dateformat"yyyy-mm-ddTHH:MM:SSzzzz")
+        catch
+            ZonedDateTime(t, dateformat"yyyy-mm-ddTHH:MMzzzz")
+        end for t in inputs.time
+    ]
+    return inputs
+end
+
+
 # PSA expected values
 function expected_2020()
     columns = [:elevation, :zenith, :azimuth]

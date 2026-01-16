13adf610f62bf227ab683a21bf13dc6acc97cf5a
Stefan de Lange <langestefan@msn.com>
2026-01-16 01:30:18 +0100

    Update fractional_hour function to use periods value

--- src/Positioning/utils.jl
+++ src/Positioning/utils.jl
@@ -1,7 +1,7 @@
 """Utility functions to be used across solar positioning algorithms."""
 
-# dt.instant.value = milliseconds since epoch
-@inline fractional_hour(dt::DateTime) = (dt.instant.value % 86_400_000) / 3_600_000
+# dt.instant.periods.value = milliseconds since epoch
+@inline fractional_hour(dt::DateTime) = (dt.instant.periods.value % 86_400_000) / 3_600_000
 
 # constants
 const EMR = 6371.01  # Earth Mean Radius in km
